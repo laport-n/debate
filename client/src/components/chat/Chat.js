ea71c66bb64262938321f2f6d0b43c1fa1d0bd20
laport-n <nicolas-1301@live.fr>
2020-10-07 17:22:45 +0200

    show state on client

---
+++ client/src/components/chat/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Chat.css';
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
---
+++ client/src/components/displayCam/DisplayCam.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DisplayCam.css';
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
